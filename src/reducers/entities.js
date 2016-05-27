db4b10a696c9b6428dab369a56e83f0cd03f4614
Alexander Schmidt <alexanderschmidt1@gmail.com>
2016-05-27 22:07:33 +0200

    Extend state shape of entities

--- src/reducers/entities.js
+++ src/reducers/entities.js
@@ -1,7 +1,10 @@
 import { handleActions } from 'redux-actions';
+import R from 'ramda';
 
 export default handleActions({
-    ADD_ENTITY: (state, action) => [
-        ...state, { name: action.payload }
-    ]
-}, []);
+    ADD_ENTITY: (state, action) => {
+        const items = R.uniq([ ...state.items, { name: action.payload } ]);
+
+        return R.merge(state, { items });
+    }
+}, { status: '', items: [] });
--- test/unit/reducers/entitiesSpec.js
+++ test/unit/reducers/entitiesSpec.js
@@ -14,16 +14,16 @@ describe('entities', function () {
         it('should create an entity on ADD_ENTITY getting passed an undefined initial state', () => {
             const initialState = undefined;
             const name = 'any entity name';
-            const expectedState = [ { name } ];
+            const expectedState = { status: '', items: [ { name } ] };
             const action = addEntity(name);
 
             expect(entities(initialState, action)).to.deep.equal(expectedState);
         });
 
         it('should create an entity on ADD_ENTITY getting passed an empty initial state', () => {
-            const emptyInitialState = [];
+            const emptyInitialState = { status: '', items: [] };
             const name = 'any entity name';
-            const expectedState = [ { name } ];
+            const expectedState = { status: '', items: [ { name } ] };
             const action = addEntity(name);
 
             expect(entities(emptyInitialState, action)).to.deep.equal(expectedState);
