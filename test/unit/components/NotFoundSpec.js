d1943d6f1d9f782337cd89679dcff6e0564e7484
Alexander Schmidt <alexanderschmidt1@gmail.com>
2016-05-30 10:04:30 +0200

    Add Not Found route

---
+++ test/unit/components/NotFoundSpec.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+import { PageHeader } from 'react-bootstrap';
+import NotFound from '../../../src/components/NotFound';
+
+describe('NotFound', function () {
+    function createComponent() {
+        return <NotFound />;
+    }
+
+    it('should render the page header for NotFound', function () {
+        const notFound = shallow(createComponent());
+
+        expect(notFound.find(PageHeader).children().text()).to.equal('Page not found');
+    });
+});
