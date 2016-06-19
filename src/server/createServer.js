1ac6183d5848e18e9bb5e9744c7eb4fb7a7f0255
Alexander Schmidt <alexanderschmidt1@gmail.com>
2016-06-19 16:43:57 +0200

    Configure express middleware for handling json in requests

--- src/server/createServer.js
+++ src/server/createServer.js
@@ -1,9 +1,11 @@
+import bodyParser from 'body-parser';
 import createEntitiesRouter from './createEntitiesRouter';
 
 // eslint-disable-next-line max-statements
 const createServer = (expressServer, apiPath, data = {}) => {
     const { entities } = data;
 
+    expressServer.use(bodyParser.json());
     expressServer.use(`${apiPath}/entities`, createEntitiesRouter(entities));
 
     return expressServer;
