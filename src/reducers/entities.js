0fe630edcaa7776bb24f3acf26f4870f24a376d6
Alexander Schmidt <alexanderschmidt1@gmail.com>
2016-06-20 11:08:06 +0200

    Use object notation instead of switch case

--- src/reducers/entities.js
+++ src/reducers/entities.js
@@ -3,14 +3,14 @@ import R from 'ramda';
 import * as types from '../actions/types';
 
 const items = (state = [], action) => {
-    switch (action.type) {
-    case types.ADD_ENTITY_SUCCESS:
-        return [ ...state, action.entity ];
-    case types.FETCH_ENTITIES_SUCCESS:
-        return R.uniq([ ...state, ...action.items ]);
-    default:
-        return state;
-    }
+    const handlers = {
+        [types.ADD_ENTITY_SUCCESS]: () => [ ...state, action.entity ],
+        [types.FETCH_ENTITIES_SUCCESS]: () => [ ...state, ...action.items ]
+    };
+
+    const handleAction = handlers[action.type];
+
+    return handleAction ? handleAction() : state;
 };
 
 const ids = (state = [], action) => {
@@ -30,9 +30,9 @@ const isInProgress = (state = false, action) => {
 
     const handlers = {
         [types.ADD_ENTITY_REQUEST]: inProgress,
-        [types.FETCH_ENTITIES_REQUEST]: inProgress,
         [types.ADD_ENTITY_SUCCESS]: notInProgress,
         [types.ADD_ENTITY_FAILURE]: notInProgress,
+        [types.FETCH_ENTITIES_REQUEST]: inProgress,
         [types.FETCH_ENTITIES_SUCCESS]: notInProgress,
         [types.FETCH_ENTITIES_FAILURE]: notInProgress
     };
@@ -49,9 +49,9 @@ const errorMessage = (state = null, action) => {
     const handlers = {
         [types.ADD_ENTITY_REQUEST]: noError,
         [types.ADD_ENTITY_SUCCESS]: noError,
+        [types.ADD_ENTITY_FAILURE]: handleError,
         [types.FETCH_ENTITIES_REQUEST]: noError,
         [types.FETCH_ENTITIES_SUCCESS]: noError,
-        [types.ADD_ENTITY_FAILURE]: handleError,
         [types.FETCH_ENTITIES_FAILURE]: handleError
     };
 
